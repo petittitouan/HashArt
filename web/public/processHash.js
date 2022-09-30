609ed7adafbd6c5349430b1d8f5d5e770a971305
Titouan Petit <petit.titouan1@gmail.com>
2022-09-30 16:05:07 +0200

    Prettier Formatting

--- web/public/processHash.js
+++ web/public/processHash.js
@@ -9,15 +9,19 @@ if (!hash) {
 }
 
 const hashSplit = hash.match(/.{1,6}/g)
+
 // If iterations is 0 then we put it at 1 (if we don't it would result in a blank canvas)
-const iterations = parseInt(hashSplit[hashSplit.length - 1], 16) == 0 ? 1 : parseInt(hashSplit[hashSplit.length - 1], 16)
+const iterations =
+    parseInt(hashSplit[hashSplit.length - 1], 16) == 0
+        ? 1
+        : parseInt(hashSplit[hashSplit.length - 1], 16)
 
 const canvas = document.getElementById('hashProcessingCanvas')
 const context = canvas.getContext('2d')
 
 ;(async () => {
     let currentHash = hash
-    for (let it = 0; it < iterations; it ++) {
+    for (let it = 0; it < iterations; it++) {
         fillWithHash(currentHash, context)
         currentHash = await sha512(currentHash)
     }
@@ -31,15 +35,22 @@ const context = canvas.getContext('2d')
  */
 function fillWithHash(hash, canvasContext) {
     const hashSplit = hash.match(/.{1,6}/g)
-    const colors = hashSplit.slice(0, - 1).map((color) => `#${color}`)
+    const colors = hashSplit.slice(0, -1).map((color) => `#${color}`)
     /** @type {int[]} */
-    const xCoords = hashSplit.slice(0, - 1).map((hex) => parseInt(hex.slice(0, 2), 16) + parseInt(hex.slice(2, 4), 16) + parseInt(hex.slice(4, 6), 16))
+    const xCoords = hashSplit
+        .slice(0, -1)
+        .map(
+            (hex) =>
+                parseInt(hex.slice(0, 2), 16) +
+                parseInt(hex.slice(2, 4), 16) +
+                parseInt(hex.slice(4, 6), 16)
+        )
 
     for (let i = 0; i < 210; i += 10) {
         canvasContext.fillStyle = colors[i / 10]
         // We use the color at index i / 10
         // We use the x coordinate at index i / 10 REVERSED (first color with last xCoord)
-        canvasContext.fillRect(xCoords[xCoords.length - (i / 10) - 1], i, 10, 10)
+        canvasContext.fillRect(xCoords[xCoords.length - i / 10 - 1], i, 10, 10)
     }
 }
 
